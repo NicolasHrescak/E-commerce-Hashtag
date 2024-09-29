6fb0cad2c85d7717e4eb53a418943b6329b3f813
NicolasHrescak <131788252+NicolasHrescak@users.noreply.github.com>
2024-09-29 02:52:25 -0300

    Códigos do site

---
+++ main.js
@@ -0,0 +1,8 @@
+import { renderizarCatalogo } from "./src/cartaoproduto";
+import { inicializarCarrinho } from "./src/menuCarrinho";
+import { renderizarProdutosCarrinho } from "./src/menuCarrinho";
+import { atualizarPrecoCarrinho } from "./src/menuCarrinho";
+renderizarCatalogo();
+inicializarCarrinho();
+renderizarProdutosCarrinho();
+atualizarPrecoCarrinho();
